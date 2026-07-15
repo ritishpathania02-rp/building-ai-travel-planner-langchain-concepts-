cbb5e1322addd07c725d2b261a57cd02f8257760
RITISH PATHANIA <ritishpathania02@gmail.com>
2026-07-15 14:56:12 +0530

    Refactor chain.py to include multiple prompts

--- chain.py
+++ chain.py
@@ -1,7 +1,9 @@
 from langchain_ollama import ChatOllama as co
 from langchain_core.output_parsers import StrOutputParser
-from  prompt import ai_prompt
+from  prompt import itinerary_prompt, budget_prompt, tips_prompt
 llm = co(model = "llama3:latest", temperature = 0.3)
 parser = StrOutputParser()
 # we can also write StrOutputParser() instead of parser in our chain
-travelchain = ai_prompt|llm|parser
+chain1 = itinerary_prompt|llm|parser
+chain2 = budget_prompt|llm|parser
+chain3 = tips_prompt|llm|parser
