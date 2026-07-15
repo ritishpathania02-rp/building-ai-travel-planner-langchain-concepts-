d8b7ca2b036012985ede3813bfdd1feab40e2109
RITISH PATHANIA <ritishpathania02@gmail.com>
2026-07-15 14:57:02 +0530

    Refactor travel prompts for itinerary generation
    Updated prompts for travel itinerary generation, separating itinerary, budget, and tips into distinct templates.

--- prompt.py
+++ prompt.py
@@ -1,19 +1,56 @@
 from langchain_core.prompts import ChatPromptTemplate as cpt
-ai_prompt = cpt.from_template("""
+# ai_prompt = cpt.from_template("""
+# You are an expert travel planner.
+
+# Create a travel itinerary based on the following details:
+
+# Destination: {destination}
+# Number of Days: {days}
+# Budget: {budget}
+# Interests: {interests}
+# Travel Date: {travel_date}
+
+# Provide:
+
+# 1. Day-wise itinerary
+# 2. Estimated budget breakdown
+# 3. Packing suggestions
+# 4. Local travel tips
+# """)
+
+itinerary_prompt = cpt.from_template("""
 You are an expert travel planner.
 
-Create a travel itinerary based on the following details:
-
 Destination: {destination}
-Number of Days: {days}
+Days: {days}
 Budget: {budget}
 Interests: {interests}
 Travel Date: {travel_date}
 
+Generate a detailed day-wise itinerary and you have only generate itinerary , and there sould no budget and tips.
+""")
+
+budget_prompt = cpt.from_template("""
+    "destination": {destination},
+    "days": {days},
+    "budget": {budget},
+    "interests": {interests}
+
+Estimate the budget in detail.
+
+Include:
+- Hotel
+- Food
+- Transport
+- Activities
+""")
+
+tips_prompt = cpt.from_template("""
+Destination: {destination}
+
 Provide:
 
-1. Day-wise itinerary
-2. Estimated budget breakdown
-3. Packing suggestions
-4. Local travel tips
+- Packing suggestions
+- Safety tips
+- Local travel tips
 """)
